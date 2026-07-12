3fdf748c4f994bc1c0d4f234821fa6bc98d67375
massah <arafeh.massah2007@gmail.com>
2026-07-12 12:34:21 -0400

    Initial Commit

---
+++ src/voxelize.py
@@ -0,0 +1,22 @@
+import argsparse
+from pathlib import Path
+
+import numpy as np
+import trimesh
+from tqdm import tqdm
+
+
+def normalize_mesh(mesh: trimesh.Trimesh) -> trimesh.Trimesh:
+    pass
+
+
+def mesh_to_voxel(mesh: trimesh.Trimesh, resolution: int = 32) -> np.ndarray:
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
